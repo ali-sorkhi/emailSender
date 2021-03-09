68d8149c63e338592e69ab7c3fde33be3fff6b05
ali-sorkhi <ali.sorkhi92@gmail.com>
2021-03-09 11:23:01 +0330

    google auth

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 const port =process.env.PORT || 5000;
- 
-app.get('/', (req, res) => res.send({hi: 'there'}));
+
+require('./routes/authRoutes')(app);
+
+
+//app.get('/', (req, res) => res.send({hi: 'there'}));
 app.listen(port);
