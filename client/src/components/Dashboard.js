b96579c7083b68072b73bb0ab28349d8aa71711c
ali-sorkhi <ali.sorkhi92@gmail.com>
2021-03-27 10:57:28 +0430

    Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Dashboard = () => {
     return(
         <div>
             Dashboard
+            <div className="fixed-action-btn">
+                <Link to="/surveys/new" class="btn-floating btn-large waves-effect waves-light red">
+                    <i class="material-icons">add</i>
+                </Link>
+            </div>
         </div>
     );
 };
